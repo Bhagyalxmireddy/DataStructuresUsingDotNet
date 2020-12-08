42b389e60f8c50efa7e860fa092db6ae76c40b1a
Bhagi <puppyreddy1997@gmail.com>
2020-12-08 09:35:32 +0530

    [Bhagyalaxmi]ADDED:Addes testcases and Enqueue method to instert the elements into queue

--- Stack_Queue_Testing/UnitTest1.cs
+++ Stack_Queue_Testing/UnitTest1.cs
@@ -34,5 +34,33 @@ namespace Stack_Queue_Testing
             int peek = stack.peek();
             Assert.AreEqual(peek, secondElement);
         }
+        [TestMethod]
+        public void givenElement_CheckingFortheEmpty()
+        {
+            int firstElement = 70;
+            int secondElement = 30;
+            int thirdElement = 56;
+
+            stack.PushElements(firstElement);
+            stack.PushElements(secondElement);
+            stack.PushElements(thirdElement);
+            stack.Empty_TheStack();
+            int size = stack.size();
+            Assert.AreEqual(0, size);
+        }
+        [TestMethod]
+        public void givenElement_CheckingTheStackIsEmpty()
+        {
+            int firstElement = 70;
+            int secondElement = 30;
+            int thirdElement = 56;
+
+            stack.PushElements(firstElement);
+            stack.PushElements(secondElement);
+            stack.PushElements(thirdElement);
+            stack.Empty_TheStack();
+            bool result = stack.Is_Empty();
+            Assert.IsTrue(result);
+        }
     }
 }
